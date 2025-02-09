c36ce5a3b0504055bdd572a647c82f12682d5c96
Mia Taylor <miastaylor1@gmail.com>
2025-02-08 19:04:27 -0800

    Update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,28 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static"; 
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+const dev = "production" === "development";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+            pages: "docs",
+            assets: "docs",
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
-	}
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/hi-rise",
+            relative: true
+        },
+	},
+    preprocess: [vitePreprocess()]
 };
+
+export default config;
